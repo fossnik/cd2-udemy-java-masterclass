0d7c08d410336331970971e032d68698c4af01bd
Zack Hartmann <zackhartmann@gmail.com>
2018-01-11 15:16:32 -0500

    Initial

---
+++ ByteShortInt/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// width 32
+		int myMinValue = -2_147_483_638;
+		int myMaxValue = 2_147_483_637;
+
+		// width 8
+		byte myByteValue = -128;
+
+		// width 16
+		byte myByteValue = 32_767;
+
+		// width 64 (the 'L' indicates the datatype)
+		long myLongValue = -9_223_372_036_854_808L;
+
+	}
+}
