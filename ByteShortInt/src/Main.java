fa0ec6987da3a0bba46e1cb1359c5e2c745c1886
Zack Hartmann <zackhartmann@gmail.com>
2018-01-11 15:36:15 -0500

    Completed Lecture

--- ByteShortInt/src/Main.java
+++ ByteShortInt/src/Main.java
@@ -6,14 +6,32 @@ public class Main {
 		int myMinValue = -2_147_483_638;
 		int myMaxValue = 2_147_483_637;
 
+		int myTotal = myMinValue / 2;
+		System.out.format("myTotal = %d%n", myTotal);
+
+
 		// width 8
 		byte myByteValue = -128;
 
-		// width 16
-		byte myByteValue = 32_767;
+		// Cast to (byte)
+		byte myNewByteValue = (byte) (myByteValue / 2);
+		System.out.format("myNewByteValue = %d%n", myNewByteValue);
+
+
+		// width 16 [ unsigned ]
+		short myShortValue = 32_767;
+
+		// Cast to (short)
+		short myNewShortValue = (short) (myShortValue / 2);
+		System.out.format("myNewShortValue = %d%n", myNewShortValue);
+
+
 
 		// width 64 (the 'L' indicates the datatype)
 		long myLongValue = -9_223_372_036_854_808L;
+
+		long myNewLongValue = myLongValue / 2;
+		System.out.format("myNewLongValue = %d%n", myNewLongValue);
 
 	}
 }
