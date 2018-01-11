2fe8271951cc9f8e59511cd2c261fb288b08af83
Zack Hartmann <zackhartmann@gmail.com>
2018-01-11 17:58:34 -0500

    Completed Challenge

---
+++ FloatAndDouble/src/Challenge.java
@@ -0,0 +1,10 @@
+public class Challenge {
+	public static void main(String[] args) {
+		// convert pounds to kilograms
+
+		int pounds = 1;
+		double kilograms = pounds * .45359237d;
+
+		System.out.printf("Kilograms in %d: %f", pounds, kilograms);
+	}
+}
