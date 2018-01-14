843cf8d36ac7045de136f067a2585f19a5a4e8a1
Zack Hartmann <zackhartmann@gmail.com>
2018-01-13 21:49:29 -0500

    Directory Restructure

--- 4/FloatAndDouble/src/FloatAndDouble.java
+++ 4/FloatAndDouble/src/FloatAndDouble.java
@@ -12,8 +12,12 @@ public class FloatAndDouble {
 		// width 64
 		double	myDoubleValue	= 5d / 3d;	// myDoubleValue: 1.6666666666666667
 
+		// width 64
+		long	myLongValue		= 5l / 3l;
+
 		System.out.println("myIntValue: " + myIntValue);
 		System.out.println("myFloatValue: " + myFloatValue);
 		System.out.println("myDoubleValue: " + myDoubleValue);
+		System.out.println("myLongValue: " + myLongValue);
 	}
 }
