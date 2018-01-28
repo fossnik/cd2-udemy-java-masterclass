d91d5a4af4a090f63d6a0b50cebaaa87a108d796
Zack Hartmann <zackhartmann@gmail.com>
2018-01-28 11:51:03 -0500

    Corrections to Challenge

--- 8/Polymorphism/src/Challenge.java
+++ 8/Polymorphism/src/Challenge.java
@@ -1,74 +1,73 @@
+class Car {
+	private boolean engine;
+	private int cylinders;
+	private int wheels;
+	private String name;
+
+	public Car(String name, int cylinders) {
+		this.engine = true;
+		this.cylinders = cylinders;
+		this.wheels = 4;
+		this.name = name;
+	}
+
+	public boolean isEngine() {
+		return engine;
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+
+	public int getWheels() {
+		return wheels;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void startEngine() {
+		System.out.println("Engine Started!");
+	}
+
+	public void accelerate() {
+		System.out.println("Car Accelerated!");
+	}
+
+	public void brake() {
+		System.out.println("Car Braked!");
+	}
+}
+
+class Altima extends Car {
+	public Altima(String name, int cylinders) {
+		super(name, cylinders);
+	}
+}
+
+class Defender extends Car {
+	public Defender(String name, int cylinders) {
+		super(name, cylinders);
+	}
+}
+
+class Plymouth extends Car {
+	public Plymouth(String name, int cylinders) {
+		super(name, cylinders);
+	}
+}
+
 public class Challenge {
-	public class Car {
-		private boolean engine;
-		private int cylinders;
-		private int wheels;
-		private String name;
-
-		private Car(String name, int cylinders) {
-			this.engine = true;
-			this.cylinders = cylinders;
-			this.wheels = 4;
-			this.name = name;
-		}
-
-		public boolean isEngine() {
-			return engine;
-		}
-
-		public int getCylinders() {
-			return cylinders;
-		}
-
-		public int getWheels() {
-			return wheels;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void startEngine() {
-			System.out.println("Engine Started!");
-		}
-
-		public void accelerate() {
-			System.out.println("Car Accelerated!");
-		}
-
-		public void brake() {
-			System.out.println("Car Braked!");
-		}
-	}
-
-	class Altima extends Car {
-		private Altima() {
-			super("Altima", 4);
-		}
-	}
-
-	class Defender extends Car {
-		private Defender() {
-			super("Defender", 6);
-		}
-
-	}
-
-	class Plymouth extends Car {
-		private Plymouth() {
-			super("Plymouth", 6);
-		}
-
-	}
 
 	public static void main(String[] args) {
-		Car car1 = new Altima();
-		Car car2 = new Defender();
-		Car car3 = new Plymouth();
+		Altima car1 = new Altima("Altima", 4);
+		Defender car2 = new Defender("Defender", 6);
+		Plymouth car3 = new Plymouth("Plymouth", 6);
 
 		System.out.printf("The %s has %s cylinders\n", car1.getName(), car1.getCylinders());
 		System.out.printf("Starting the %s ..\n", car2.getName());
 		System.out.printf("The %s has %s wheels\n", car3.getName(), car3.getWheels());
-
 	}
 }
+
