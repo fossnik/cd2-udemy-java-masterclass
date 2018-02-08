b78e182bae2701d4eb79859108770d5ea4baf148
Zack Hartmann <zackhartmann@gmail.com>
2018-02-08 12:04:58 -0500

    Completed Lecture 3

--- 9/LinkedList/src/Demo.java
+++ 9/LinkedList/src/Demo.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
 	public static void main(String[] args) {
@@ -48,11 +49,82 @@ public class Demo {
 				return true;
 			} else if (comparison < 0) {
 				// move on next city
-				continue;
 			}
 		}
 
 		stringListIterator.add(newCity);
 		return true;
 	}
+
+	private static void visit(LinkedList cities) {
+		Scanner scanner = new Scanner(System.in);
+		boolean quit = false;
+		boolean goingForward = true;
+		ListIterator<String> listIterator = cities.listIterator();
+
+		if(cities.isEmpty()) {
+			System.out.println("No cities in the itenerary");
+			return;
+		} else {
+			System.out.println("Now visiting " + listIterator.next());
+			printMenu();
+		}
+
+		while (!quit) {
+			int action = scanner.nextInt();
+			scanner.nextLine();
+			switch(action) {
+				case 0:
+					System.out.println("Holiday (Vacation) over");
+					quit = true;
+					break;
+
+				case 1:
+					if(!goingForward) {
+						if(listIterator.hasNext()) {
+							listIterator.next();
+						}
+						goingForward = true;
+					}
+					if(listIterator.hasNext()) {
+						System.out.println("Now visiting " + listIterator.next());
+					} else {
+						System.out.println("Reached the end of the list");
+						goingForward = false;
+					}
+					break;
+
+				case 2:
+					if(goingForward) {
+						if(listIterator.hasPrevious()) {
+							listIterator.previous();
+						}
+						goingForward = false;
+					}
+					if(listIterator.hasPrevious()) {
+						System.out.println("Now visiting " + listIterator.previous());
+					} else {
+						System.out.println("We are at the start of the list");
+						goingForward = true;
+					}
+					break;
+
+				case 3:
+					printMenu();
+					break;
+
+			}
+
+		}
+
+	}
+
+	private static void printMenu() {
+		System.out.println("Available actions:\npress ");
+		System.out.println("0 - to quit\n" +
+				"1 - go to next city\n" +
+				"2 - go to previous city\n" +
+				"3 - print menu options");
+	}
+
 }
