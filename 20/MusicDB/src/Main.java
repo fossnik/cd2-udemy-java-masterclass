a756734ded3b4e9a0dd855ac9a4acf6462d8539e
Zack Hartmann <zackhartmann@gmail.com>
2018-02-19 12:39:34 -0500

    Completed Lecture

---
+++ 20/MusicDB/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Datasource datasource = new Datasource();
+		if(!datasource.open()) {
+			System.out.println("Can't open datasource");
+			return;
+		}
+
+		datasource.close();
+	}
+}
